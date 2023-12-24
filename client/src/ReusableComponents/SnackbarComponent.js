d4c74d923ac4c1c55ecca672695fb3047da3da8f
8maaan <johnrandolf696969@gmail.com>
2023-12-24 17:12:08 +0800

    Snackbar variant change

--- client/src/ReusableComponents/SnackbarComponent.js
+++ client/src/ReusableComponents/SnackbarComponent.js
@@ -4,7 +4,7 @@ export default function SnackbarComponent({open, onClose, severity, message}) {
     return(
         <div>
           <Snackbar open={open} autoHideDuration={2000} onClose={onClose} anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}>
-            <Alert onClose={onClose} severity={severity} sx={{ width: '100%' }}>
+            <Alert variant="filled" onClose={onClose} severity={severity} sx={{ width: '100%' }}>
               {message}
             </Alert>
           </Snackbar>
